7fdbd0cb3299fdc580f0f104e57cc4dd99c4b52c
Dmytro-Krasyliuk <50119871+Dmytro-Krasyliuk@users.noreply.github.com>
2023-05-18 00:13:00 +0300

    в
    в

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-window.Telegram.WebApp.MainButton.isVisible = true;
 window.Telegram.WebApp.MainButton.setText("Відповісти");
 window.Telegram.WebApp.MainButton.onclick(() => {
   window.Telegram.WebApp.showPopup({
@@ -6,5 +5,7 @@ window.Telegram.WebApp.MainButton.onclick(() => {
     message: "Правильно! +10 балів!",
   });
 });
+
+window.Telegram.WebApp.MainButton.isVisible = true;
 
 console.log(window.Telegram.WebApp.initData);
