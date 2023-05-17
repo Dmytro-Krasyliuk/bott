514617e7682671449cbaa33b3a73026718cc7cc1
Dmytro-Krasyliuk <50119871+Dmytro-Krasyliuk@users.noreply.github.com>
2023-05-17 23:46:18 +0300

    df
    df

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 window.Telegram.WebApp.MainButton.isVisible = true;
+window.Telegram.WebApp.MainButton.onclick(() => {
+    window.Telegram.WebApp.showPopup({
+      title: "hello",
+      message: "super",
+      buttons: [
+        { type: "close", text: "CLOSE" },
+        { type: "destructive", text: "deletos" },
+      ],
+    });
+
+})
+
 console.log(window.Telegram.WebApp.initData);
-window.Telegram.WebApp.showPopup({
-  title: "hello",
-  message: "super",
-});
+
