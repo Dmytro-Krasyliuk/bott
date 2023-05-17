f20c46d83ac374155786fa0e37d8675b89436651
Dmytro-Krasyliuk <50119871+Dmytro-Krasyliuk@users.noreply.github.com>
2023-05-18 00:10:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 window.Telegram.WebApp.MainButton.isVisible = true;
+window.Telegram.WebApp.MainButton.setText("Відповісти");
 window.Telegram.WebApp.MainButton.onclick(() => {
-    window.Telegram.WebApp.showPopup({
-      title: "hello",
-      message: "super",
-      buttons: [
-        { type: "close", text: "CLOSE" },
-        { type: "destructive", text: "deletos" },
-      ],
-    });
-
-})
+  window.Telegram.WebApp.showPopup({
+    title: "hello",
+    message: "Правильно! +10 балів!",
+  });
+});
 
 console.log(window.Telegram.WebApp.initData);
-
