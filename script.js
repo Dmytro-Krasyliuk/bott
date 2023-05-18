575dc00f630ba7651839464b6afe1ae325b6cf7f
Dmytro-Krasyliuk <50119871+Dmytro-Krasyliuk@users.noreply.github.com>
2023-05-18 13:28:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // tg.ready();
-// let tg = window.Telegram.WebApp;
-// tg.MainButton.setText("Відповісти");
-// tg.MainButton.isVisible = true;
+let tg = window.Telegram.WebApp;
+tg.MainButton.setText("Відповісти");
+tg.MainButton.isVisible = true;
 // document.addEventListener("DOMContentLoaded", () => {
 //   let nameProfile = document.getElementsByClassName("profile-name")[0];
 //   let avaProfile = document.getElementsByClassName("profile-ava")[0];
