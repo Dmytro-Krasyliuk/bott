24bc0ebfeaacfbb3f8e29baf6818b464d8817d0a
Dmytro-Krasyliuk <50119871+Dmytro-Krasyliuk@users.noreply.github.com>
2023-05-18 12:42:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,16 +8,19 @@ Telegram.WebApp.onEvent("mainButtonClicked", function () {
   tg.sendData("some string that we need to send");
   //при клике на основную кнопку отправляем данные в строковом виде
 
-  tg.showPopup({
-    title: "hello1",
-    message: "Правильно! +10 балів!",
-  });
+  // tg.showPopup({
+  //   title: "hello1",
+  //   message: "Правильно! +10 балів!",
+  // });
+
+  let nameProfile = document.getElementsByClassName("profile-name")[0];
+  nameProfile.textContent = tg.initDataUnsafe?.user?.username;
+
 });
 
 tg.MainButton.isVisible = true;
 
-console.log(tg.initData);
-document.addEventListener("DOMContentLoaded", () => {
-  let nameProfile = document.getElementsByClassName("profile-name")[0];
-  nameProfile.textContent = tg.initDataUnsafe?.user?.username;
-});
+// console.log(tg.initData);
+
+// let nameProfile = document.getElementsByClassName("profile-name")[0];
+// nameProfile.textContent = tg.initDataUnsafe?.user?.username;
