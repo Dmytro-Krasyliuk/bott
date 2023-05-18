503a1a4991e788bfe431d7abed2e9f1a74909f9a
Dmytro-Krasyliuk <50119871+Dmytro-Krasyliuk@users.noreply.github.com>
2023-05-18 13:44:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-tg.ready();
 let tg = window.Telegram.WebApp;
-tg.MainButton.setText("Відповісти");
-tg.MainButton.isVisible = true;
+
 document.addEventListener("DOMContentLoaded", () => {
   let nameProfile = document.getElementsByClassName("profile-name")[0];
   let avaProfile = document.getElementsByClassName("profile-ava")[0];
   nameProfile.textContent = tg.initDataUnsafe?.user?.username;
   avaProfile.src = tg.initDataUnsafe?.user?.photo_url;
 });
+
+tg.ready();
+tg.MainButton.setText("Відповісти");
+tg.MainButton.isVisible = true;
