250009c35594a89e5c706bc7d820ac6387b19bb7
Dmytro-Krasyliuk <50119871+Dmytro-Krasyliuk@users.noreply.github.com>
2023-05-18 14:02:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ tg.MainButton.setText("Відповісти");
 tg.MainButton.isVisible = true;
 
 let nameProfile = document.getElementsByClassName("profile-name")[0];
-nameProfile.textContent = tg.initDataUnsafe?.user?.username;
-
-let avaProfile = document.getElementsByClassName("profile-ava")[0];
+// nameProfile.textContent = tg.initDataUnsafe?.user?.username;
 nameProfile.textContent = JSON.stringify(tg.initDataUnsafe?.user);
+let avaProfile = document.getElementsByClassName("profile-ava")[0];
 avaProfile.src = tg.initDataUnsafe?.user?.photo_url;
 
 
