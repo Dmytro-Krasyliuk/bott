a453996c3161a77aea21f505744e7a925cb92395
Dmytro-Krasyliuk <50119871+Dmytro-Krasyliuk@users.noreply.github.com>
2023-05-18 13:57:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let nameProfile = document.getElementsByClassName("profile-name")[0];
 nameProfile.textContent = tg.initDataUnsafe?.user?.username;
 
 let avaProfile = document.getElementsByClassName("profile-ava")[0];
-nameProfile.textContent = tg.initDataUnsafe?.user?.photo_url;
+nameProfile.textContent = JSON.stringify(tg.initDataUnsafe?.user);
 avaProfile.src = tg.initDataUnsafe?.user?.photo_url;
 
 
