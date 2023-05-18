b408bf3cd89dcaa6fe94ae69ef9705ae38896ab2
Dmytro-Krasyliuk <50119871+Dmytro-Krasyliuk@users.noreply.github.com>
2023-05-18 12:56:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 let tg = window.Telegram.WebApp;
-
+let nameProfile = document.getElementsByClassName("profile-name")[0];
+nameProfile.textContent = tg.initDataUnsafe?.user?.username;
+photo_url;
 tg.MainButton.isVisible = true;
 
 tg.MainButton.setText("Відповісти");
+tg.ready();
 
-Telegram.WebApp.onEvent("mainButtonClicked", function () {
-  // tg.sendData("some string that we need to send");
-  //при клике на основную кнопку отправляем данные в строковом виде
+// Telegram.WebApp.onEvent("mainButtonClicked", function () {
+//   // tg.sendData("some string that we need to send");
+//   //при клике на основную кнопку отправляем данные в строковом виде
 
-  // tg.showPopup({
-  //   title: "hello1",
-  //   message: "Правильно! +10 балів!",
-  // });
+//   // tg.showPopup({
+//   //   title: "hello1",
+//   //   message: "Правильно! +10 балів!",
+//   // });
 
-  let nameProfile = document.getElementsByClassName("profile-name")[0];
-  nameProfile.textContent = tg.initDataUnsafe?.user?.username;
-});
-document.addEventListener("DOMContentLoaded", () => {
-  tg.ready();
-});
+//   let nameProfile = document.getElementsByClassName("profile-name")[0];
+//   nameProfile.textContent = tg.initDataUnsafe?.user?.username;
+
+// });
+// document.addEventListener("DOMContentLoaded", () => {
+//   tg.ready();
+// });
 
 // console.log(tg.initData);
 
