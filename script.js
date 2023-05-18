03b3beb0a88608b3a9da53ccaaaaf53205dd3dbb
Dmytro-Krasyliuk <50119871+Dmytro-Krasyliuk@users.noreply.github.com>
2023-05-18 12:29:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,5 +19,5 @@ tg.MainButton.isVisible = true;
 console.log(tg.initData);
 document.addEventListener("DOMContentLoaded", () => {
   let nameProfile = document.getElementsByClassName("profile-name")[0];
-  nameProfile.textContent = tg.initData;
+  nameProfile.textContent = tg.WebAppInitData.user.username;
 });
